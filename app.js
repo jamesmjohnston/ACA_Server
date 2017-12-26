dfb64d16c4b2f4459b38742d46db06e2546fb055
James Johnston <topspin92435@gmail.com>
2017-12-26 10:21:48 -0800

    bugfix for route

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ var bodyParser = require('body-parser');
 
 var index = require('./routes/index');
 var users = require('./routes/users');
-var users = require('./routes/map');
+var map = require('./routes/map');
 
 var app = express();
 
