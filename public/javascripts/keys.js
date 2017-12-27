983132735923b9836e5c2cd4bdf2736845a7e098
James Johnston <topspin92435@gmail.com>
2017-12-26 21:45:07 -0800

    add move server updaes

--- public/javascripts/keys.js
+++ public/javascripts/keys.js
@@ -4,19 +4,23 @@ keys.handleKeys = function(container) {
 	if (!_.isEmpty(keyState)) {
 		if (!moving) {
 			if (keyState[37] || keyState[65]){
-				moving = true;
+				moving = true;	
+				sys.Rest('user/0');
 				createjs.Tween.get(container).to({x:container.x+GRID_SIZE}, MOVE_SPEED).call(handleComplete);
 			}     
 			else if (keyState[38] || keyState[87]){
-				moving = true;
+				moving = true;	
+				sys.Rest('user/1');
 				createjs.Tween.get(container).to({y:container.y+GRID_SIZE}, MOVE_SPEED).call(handleComplete);
 			}  
 			else if  (keyState[39] || keyState[68]){
-				moving = true;
+				moving = true;	
+				sys.Rest('user/2');
 				createjs.Tween.get(container).to({x:container.x-GRID_SIZE}, MOVE_SPEED).call(handleComplete);
 			}   
 			else if (keyState[40] || keyState[83]){
-				moving = true;
+				moving = true;	
+				sys.Rest('user/3');
 				createjs.Tween.get(container).to({y:container.y-GRID_SIZE}, MOVE_SPEED).call(handleComplete);
 			}
 		}
--- routes/users.js
+++ routes/users.js
@@ -4,6 +4,30 @@ var router = express.Router();
 router.users = {};
 /* GET users listing. */
 router.get('/', function(req, res, next) {
+	var ip = req.headers['x-forwarded-for'] || req.connection.remoteAddress;
+	console.log("moving- " + ip);
+	user = new User(ip);
+	switch (req.params) {
+		case 0:
+			user.x--;
+			break;
+		case 1:
+			user.y++;
+			break;
+		
+		case 2:
+			user.x++;
+			break;
+		
+		case 3:
+			user.y--;
+			break;
+	}
+	user.save();
+  res.send(user);
+});
+
+router.get('/move/:dir', function(req, res, next) {
   var ip = req.headers['x-forwarded-for'] || req.connection.remoteAddress;
   console.log("fetching- " + ip);
   user = new User(ip);
