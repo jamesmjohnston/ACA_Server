55426794374327d34b43d69a9f130e0ca7ca40cf
James Johnston <topspin92435@gmail.com>
2017-12-26 21:49:57 -0800

    user => users

--- public/javascripts/keys.js
+++ public/javascripts/keys.js
@@ -5,22 +5,22 @@ keys.handleKeys = function(container) {
 		if (!moving) {
 			if (keyState[37] || keyState[65]){
 				moving = true;	
-				sys.Rest('user/move/0');
+				sys.Rest('users/move/0');
 				createjs.Tween.get(container).to({x:container.x+GRID_SIZE}, MOVE_SPEED).call(handleComplete);
 			}     
 			else if (keyState[38] || keyState[87]){
 				moving = true;	
-				sys.Rest('user/move/1');
+				sys.Rest('users/move/1');
 				createjs.Tween.get(container).to({y:container.y+GRID_SIZE}, MOVE_SPEED).call(handleComplete);
 			}  
 			else if  (keyState[39] || keyState[68]){
 				moving = true;	
-				sys.Rest('user/move/2');
+				sys.Rest('users/move/2');
 				createjs.Tween.get(container).to({x:container.x-GRID_SIZE}, MOVE_SPEED).call(handleComplete);
 			}   
 			else if (keyState[40] || keyState[83]){
 				moving = true;	
-				sys.Rest('user/move/3');
+				sys.Rest('users/move/3');
 				createjs.Tween.get(container).to({y:container.y-GRID_SIZE}, MOVE_SPEED).call(handleComplete);
 			}
 		}
