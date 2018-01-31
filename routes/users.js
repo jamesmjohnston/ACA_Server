0371126e589920c2364be4589521f4bf92a8121d
EC2 Default User <ec2-user@ip-172-31-2-114.us-west-1.compute.internal>
2018-01-31 08:35:49 +0000

    formatting

--- routes/users.js
+++ routes/users.js
@@ -4,36 +4,35 @@ module.exports = function(polls, map, User) {
 
 	router.users = {};
 	router.get('/move/:dir', function(req, res, next) {
-		var id = req.headers.authorization;
-		var user = new User(id);
+        var id = req.headers.authorization;
+        var user = new User(id);
 
         // move user
         user.move(parseInt(req.params.dir));
 
         // expand visible chunks
-		var chunks = map.getVisibleChunks(user);
-		var view = user.expandChunks(chunks, map);
+        var chunks = map.getVisibleChunks(user);
+        var view = user.expandChunks(chunks, map);
 
-		user.save();
+        user.save();
 
         // push data to relevant users
         polls.pollLocalUsers(user, router.users);
 		
         // repond with new chunks of availabe
-		res.send(view);
-	});
+        res.send(view);
+    });
 
-	
-	router.get('/', function(req, res, next) {
-	  var id = req.headers.authorization;
-	  res.send(new User(id));
-	});
 
-	router.init = function() {
+    router.get('/', function(req, res, next) {
+        var id = req.headers.authorization;
+        res.send(new User(id));
+    });
+
+    router.init = function() {
         // load user list
-		this.users = sys.loadJSON('users');
-	};
+        this.users = sys.loadJSON('users');
+    };
 
-   return router;
-
+    return router;
 }
